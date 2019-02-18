5798f530d3ae638c1ef87c03ebb89a4603de3dd7
30059200 <francisco-javier.sucre@ext.leroymerlin.es>
2019-02-18 15:42:54 +0100

    1.0.0

--- src/exceptions.ts
+++ src/exceptions.ts
@@ -7,6 +7,6 @@ export class FileNotFoundError extends Error {
   constructor(path: string) {
     super(`The requested file at path "${path}" was not found`);
     this.path = path;
-    this.message = `The requested file at path "${path}" was not found`
+    this.message = `The requested file at path "${path}" was not found`;
   }
 }
