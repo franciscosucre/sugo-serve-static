45fa0febdc09673800a6df14dfb03ea2c9ade751
30059200 <francisco-javier.sucre@ext.leroymerlin.es>
2019-04-16 17:23:39 +0200

    asdasd

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-const serveStatic = require("./serveStatic");
-const path = require("path");
+const serveStatic = require('./serveStatic');
+const path = require('path');
 
-const { createServer } = require("@sugo/server");
+const { createServer } = require('@sugo/server');
 
 const server = createServer((req, res) => console.log(req.url))
-  .useMiddleware(serveStatic({ dir: path.resolve("./dashboard"), baseUrl: "/dashboard" }))
+  .useMiddleware(serveStatic({ dir: path.resolve('./dashboard'), baseUrl: '/dashboard' }))
   .listen(5000);
